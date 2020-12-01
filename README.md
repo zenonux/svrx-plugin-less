a9d3bcd6ce38303110051ac170445156ef344d66
joy2019888@163.com <joy2019888@163.com>
2020-12-01 17:57:36 +0800

    init

---
+++ README.md
@@ -0,0 +1,33 @@
+svrx-plugin-less
+---
+
+[![svrx](https://img.shields.io/badge/svrx-plugin-%23ff69b4?style=flat-square)](https://svrx.io/)
+[![npm](https://img.shields.io/npm/v/svrx-plugin-less.svg?style=flat-square)](https://www.npmjs.com/package/svrx-plugin-less)
+
+The svrx plugin for less
+
+## Usage
+
+> Please make sure that you have installed [svrx](https://svrx.io/) already.
+
+### Via CLI
+
+```bash
+svrx -p less
+```
+
+### Via API
+
+```js
+const svrx = require('@svrx/svrx');
+
+svrx({ plugins: [ 'less' ] }).start();
+```
+
+## Options
+
+<!-- TODO -->
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,71 @@
+const less = require('less');
+const EventEmitter = require('events');
+const { Transform } = require('stream');
+const fs = require('fs');
+
+class LessTransform extends Transform {
+  constructor(cssPath) {
+    super({
+      transform(chunk, enc, callback) {
+        this._chunkString.push(chunk.toString());
+        callback();
+      },
+      async flush(callback) {
+        let code = this._chunkString.join('');
+        try {
+          const res = await less.render(code, {
+            paths: [process.cwd() + cssPath],
+          });
+          code = res.css;
+        } catch (error) {
+          console.warn('LessTransform', error);
+        }
+        this.push(code);
+        callback();
+      },
+    });
+
+    this._chunkString = [];
+  }
+}
+
+function isReadableStream(test) {
+  // ducking type check
+  return test instanceof EventEmitter && typeof test.read === 'function';
+}
+
+function _transform(body, cssPath) {
+  if (isReadableStream(body)) {
+    body = body.pipe(new LessTransform(cssPath));
+  }
+  return body;
+}
+
+async function onTransform(ctx, next, cssPath) {
+  await next();
+  if (/\.(css)($|\?)/.test(ctx.path)) {
+    if (!ctx.body) {
+      return;
+    }
+    const lessPath = ctx.body.path.replace(/\.css/, '.less');
+    if (fs.existsSync(lessPath)) {
+      const fileStream = fs.createReadStream(lessPath);
+      ctx.body = _transform(fileStream, cssPath);
+    }
+  }
+}
+
+module.exports = {
+  // Ref: https://docs.svrx.io/en/plugin/contribution.html#schema
+  configSchema: {},
+  hooks: {
+    // Ref: https://docs.svrx.io/en/plugin/contribution.html#server
+    async onCreate({ middleware, config }) {
+      const cssPath = config.get('path') || '/css';
+      const isBuild = config.get('build') || false;
+      middleware.add('svrx-plugin-less', {
+        onRoute: (ctx, next) => onTransform(ctx, next, cssPath, isBuild),
+      });
+    },
+  },
+};
